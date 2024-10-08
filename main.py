b61c3bca198132b357035419229c689e7411f69d
Riccardo Brugnolo <71872779+Brugi941@users.noreply.github.com>
2024-10-08 17:33:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -50,18 +50,19 @@ def generate_orders(num_companies: int, orders_per_company: int):
     for _ in range(num_companies):
         company_name = fake.company()
         for _ in range(orders_per_company):
-            order = Order(
-                order_id=order_id,
-                company_name=company_name,
-                total_order=round(random.uniform(20, 500), 2),
-                order_date=fake.date_between(start_date='-1y', end_date='today'),
-                shipping_address=fake.address().replace("\n", ", "),
-                line_items=[LineItemOrder(
+            righe =[LineItemOrder(
                     article=random.randint(1000, 9999),
                     quantity=random.randint(1, 20),
                     price=round(random.uniform(10, 200), 2),
                     requested_delivery_date=fake.date_between(start_date='today', end_date='+90d')
                 ) for _ in range(random.randint(2, 10))]
+            order = Order(
+                order_id=order_id,
+                company_name=company_name,
+                total_order=sum(c.price for c in righe),
+                order_date=fake.date_between(start_date='-1y', end_date='today'),
+                shipping_address=fake.address().replace("\n", ", "),
+                line_items=righe
             )
             orders.append(order)
             order_id += 1
@@ -72,20 +73,21 @@ def generate_invoices_from_orders(invoices_per_order: int):
     for order in orders:
         for _ in range(invoices_per_order):
             scaduto = random.choice([True, False])
-            invoice = Invoice(
-                invoice_id=invoice_id,
-                company_name=order.company_name,
-                total_amount=round(random.uniform(20, 500), 2),
-                invoice_date=fake.date_between(start_date=order.order_date, end_date='+30d'),
-                shipping_address=order.shipping_address,
-                scaduto=scaduto,
-                line_items=[LineItemInvoice(
+            righe = [LineItemInvoice(
                     article=item.article,
                     quantity=item.quantity,
                     price=item.price,
                     requested_delivery_date=item.requested_delivery_date,
                     order_reference=item
                 ) for item in order.line_items]
+            invoice = Invoice(
+                invoice_id=invoice_id,
+                company_name=order.company_name,
+                total_amount=sum(c.price for c in righe),
+                invoice_date=fake.date_between(start_date=order.order_date, end_date='+30d'),
+                shipping_address=order.shipping_address,
+                scaduto=scaduto,
+                line_items=righe
             )
             invoices.append(invoice)
             invoice_id += 1
